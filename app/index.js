bd2d875ed99aea1d5cee1f401219272374fc66ed
Joshua Fairchild <joshua.fairchild429@gmail.com>
2019-02-25 17:37:45 -0800

    uncomment the logic that tests that the build worked

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ function component () {
   const element = document.createElement('div')
 
   // lodash is used here for bundling demonstration purposes
-  // element.innerHTML = _.join(['Great', 'success,', 'Gradle!'], ' ')
+  element.innerHTML = _.join(['Great', 'success,', 'Gradle!'], ' ')
 
   return element
 }
