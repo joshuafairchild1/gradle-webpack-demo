aa05692423ca401bdac14cb82d6d9af4e833e356
Joshua Fairchild <joshua.fairchild429@gmail.com>
2019-02-25 17:22:20 -0800

    example repo to invoke webpack from gradle

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import _ from 'lodash'
+
+function component () {
+  const element = document.createElement('div')
+
+  // lodash is used here for bundling demonstration purposes
+  // element.innerHTML = _.join(['Great', 'success,', 'Gradle!'], ' ')
+
+  return element
+}
+
+document.body.appendChild(component())
